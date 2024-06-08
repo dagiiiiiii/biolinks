d50e730857457cb2dbabceea8b6b9564354f42a2
dagiiiiiii <dagiuyy111@gmail.com>
2024-06-09 06:49:00 +0700

    mengubah tampilan

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 
 
 //scroll reveal
-ScrollReveal({
-    
-    distance:'20px',
-    duration:1000,
-    delay:200
+const sr = ScrollReveal({
+    distance: '20px',
+    duration: 2000
 });
-//tambahkan kanan kiri
-ScrollReveal().reveal('.card', {origin: 'left'})
+
+// Dapatkan semua elemen dengan kelas 'card-item'
+const cardItems = document.querySelectorAll('.card-item');
+
+// Atur animasi bergantian kiri dan kanan untuk setiap elemen dan tambahkan delay
+cardItems.forEach((item, index) => {
+    const origin = (index % 2 === 0) ? 'left' : 'right';
+    const delay = 400 * (index + 1); // Tambahkan delay berdasarkan indeks item
+    sr.reveal(item, { origin: origin, delay: delay });
+});
 
 //typed
 const typed =  new Typed('.multiple-text',{
